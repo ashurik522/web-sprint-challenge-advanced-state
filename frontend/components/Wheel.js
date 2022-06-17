f7510978b61fc48ab869903cc95484381f632265
ashurik522 <ashurik522@gmail.com>
2022-06-17 08:32:38 -0400

    wheel functionality complete

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -19,12 +19,24 @@ function Wheel(props) {
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        <div className={`cog${props.wheel === 0 ? ' active' : ''}`} style={{ "--i": 0 }}>
+          {props.wheel === 0 ? 'B' : ''}
+        </div>
+        <div className={`cog${props.wheel === 1 ? ' active' : ''}`} style={{ "--i": 1 }}>
+          {props.wheel === 1 ? 'B' : ''}
+        </div>
+        <div className={`cog${props.wheel === 2 ? ' active' : ''}`} style={{ "--i": 2 }}>
+          {props.wheel === 2 ? 'B' : ''}
+        </div>
+        <div className={`cog${props.wheel === 3 ? ' active' : ''}`} style={{ "--i": 3 }}>
+          {props.wheel === 3 ? 'B' : ''}
+        </div>
+        <div className={`cog${props.wheel === 4 ? ' active' : ''}`} style={{ "--i": 4 }}>
+          {props.wheel === 4 ? 'B' : ''}
+        </div>
+        <div className={`cog${props.wheel === 5 ? ' active' : ''}`} style={{ "--i": 5 }}>
+          {props.wheel === 5 ? 'B' : ''}
+        </div>
       </div>
       <div id="keypad">
         <button onClick={counterClockwiseClick} id="counterClockwiseBtn" >Counter clockwise</button>
